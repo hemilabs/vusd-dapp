5c6b044ace785ed3bfbae0c6296e84b59133516e
Gonzalo D'Elia <gonzalo.n.delia@gmail.com>
2021-06-17 14:15:22 -0300

    Remove CORS for api tokens call (vusd dapp)

--- site/pages/api/tokens.js
+++ site/pages/api/tokens.js
@@ -1,7 +1,6 @@
 'use strict'
 import getTokensSS from '../../utils/getTokensSS'
-import { withCors } from '../../utils/withCors'
 
-export default withCors(function handler(req, res) {
+export default function handler(req, res) {
   return getTokensSS().then((tokens) => res.status(200).json(tokens))
-})
+}
