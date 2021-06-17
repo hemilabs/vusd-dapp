9056deb5b8300868a7683bd9d0f11d1f55a3962a
Gonzalo D'Elia <gonzalo.n.delia@gmail.com>
2021-06-17 12:24:37 -0300

    Use addresses from vusd-lib

--- dashboard/pages/index.js
+++ dashboard/pages/index.js
@@ -4,6 +4,7 @@ import Big from 'big.js'
 import { TokenBalances } from '../components/TokenBalances'
 import { EtherscanLink } from '../components/EtherscanLink'
 import { getAddressInfo } from '../services/info'
+import contracts from 'vusd-lib/src/contracts.json'
 import styles from '../styles/Home.module.css'
 
 const useBalance = function (address, initialData) {
@@ -30,8 +31,8 @@ const useBalance = function (address, initialData) {
   )
 }
 
-const TREASURY = '0x813E891e2Bb6729beF4185663624bd09F4902bD8'
-const MULTI_SIG_GOVERNOR = '0x6c2e3f1a88C19Bf4cf14fa38B8f745330573Da37'
+const TREASURY = contracts.Treasury
+const MULTI_SIG_GOVERNOR = contracts.MultiSigGovernor
 
 const Home = function ({ treasuryInitialData, multiSigGovernorInitialData }) {
   const { data: treasuryData } = useBalance(TREASURY, treasuryInitialData)
