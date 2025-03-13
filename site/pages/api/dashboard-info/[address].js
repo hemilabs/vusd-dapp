0d547cc7b4013d2919e5206e56a3b1683c8aa531
Gabriel Montes <gabriel@hemi.xyz>
2025-03-13 12:58:05 -0400

    Improve API checks

--- site/pages/api/dashboard-info/[address].js
+++ site/pages/api/dashboard-info/[address].js
@@ -1,6 +1,11 @@
-import { withCors } from '../../../utils/withCors'
 import { getAddressInfo } from '../../../utils/ethplorer'
+import { withCors } from '../../../utils/withCors'
 
-export default withCors((req, res) =>
-  getAddressInfo(req.query.address).then(info => res.status(200).json(info))
-)
+export default withCors(function (req, res) {
+  if (!req.query.address.test(/0x[0-9a-fA-F]{40}/)) {
+    res.status(400).json({ error: 'Invalid address' })
+  }
+  return getAddressInfo(req.query.address).then(info =>
+    res.status(200).json(info)
+  )
+})
